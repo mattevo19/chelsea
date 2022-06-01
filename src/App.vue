363fdbd747d373addfb9626ac79dd2a8f546a502
Matt Evans <mattevo10@gmail.com>
2022-06-01 10:46:02 +0200

    player list with a form

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <script setup>
+import { ref } from "vue"
+
 import Title from "./components/Title.vue"
 import Badge from "./components/Badge.vue"
-import { ref } from "vue"
-import Form from "./components/Form.vue"
+import PlayerList from "./components/PlayerList.vue"
 
-const name = ref("")
-const players = ref(["mount", "alonso", "mendy", "lukaku", "havertz", "kovacic", "kante"])
 const props = ref({
   title: "Chelsea",
   url: "https://1.bp.blogspot.com/-0oa9EtRDhwg/YDYNKk-Iv1I/AAAAAAAC5TA/8wr-e7py2E8wrJOoukbcNi8473L0CZRTgCNcBGAsYHQ/s1600/chelsea-2021-logo-erweiterung-von-footy-headlines.png"
 })
-
-const addToPlayers = (name) => {
-  players.value.push(name)
-}
 </script>
 
 <template>
   <Title :title="props.title" />
   <Badge :image="props.url" />
-
-  <ul>
-    <li v-for="player in players">{{ player }}</li>
-  </ul>
-
-  <form @submit.prevent="addToPlayers(name)">
-    <input v-model.trim="name" type="text" placeholder="Add player" />
-    <button>Add</button>
-  </form>
+  <PlayerList />
 </template>
 
 <style>
-@import "./assets/base.scss";
+@import "./styles/base.scss";
 </style>
--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -1,7 +1,23 @@
 <script setup>
-defineProps({
-  players: Array
-})
+import { ref } from "vue"
+
+const name = ref("")
+const players = ref(["mount", "alonso", "mendy", "lukaku", "havertz", "kovacic", "kante"])
+
+const addToPlayers = () => {
+  players.value.push(name.value)
+  name.value = ""
+}
+console.log()
 </script>
 
-<template></template>
+<template>
+  <ul>
+    <li v-for="player in players">{{ player }}</li>
+  </ul>
+
+  <form @submit.prevent="addToPlayers">
+    <input v-model.trim="name" type="text" placeholder="Add player" />
+    <button>Add</button>
+  </form>
+</template>
